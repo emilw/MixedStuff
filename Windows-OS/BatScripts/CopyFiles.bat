fffbb0253b0ccf88ca062f961546efeafef504f3
Emil W <emil@fornby.com>
2013-01-13 17:14:01 +0100

    Dummy update

--- Windows-OS/BatScripts/CopyFiles.bat
+++ Windows-OS/BatScripts/CopyFiles.bat
@@ -1,2 +1,2 @@
-copy /Y Meow.fr.Minifier.dll E:\MF11\Mediusflow\Frontend\Web\bin\Meow.fr.Minifier.dll
+copy /Y Meow.fr.Minifier.dll E:\MF11\Mediusflow\Frontend\Web\bin\Meow.fr.Minifier.dllx
 copy /Y Meow.fr.Minifier.dll E:\MF11\Mediusflow\Frontend\Web\bin\Meow.fr.Minifier.pbd
