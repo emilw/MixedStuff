5989cdd24973d0357a5ea219913237fc86a0c151
EmilW <emilw@konsult47.medius.local>
2015-03-22 22:18:21 +0100

    Small fix

--- SQL scripts/XIDocumentImportTimes.sql
+++ SQL scripts/XIDocumentImportTimes.sql
@@ -2,7 +2,7 @@
 --Edi documents import times
 --Assumption, diff is between doc is created and last save. The last save involves setting the doc id in the "real" flow
 --Includes all without an error
-Select 'EDI docs: ', LEFT(CONVERT(varchar, d.CreatedTimestamp,112),6) As YearMonth, AVG(DATEDIFF(MILLISECOND, d.CreatedTimestamp, d.ChangedTimestamp)) as DiffMs, COUNT(*) as NumberOfDocs
+Select 'EDI docs: ', LEFT(CONVERT(varchar, d.CreatedTimestamp,112),6) As YearMonth, AVG(DATEDIFF(MILLISECOND, d.CreatedTimestamp, d.ChangedTimestamp)) as ImportTimeMs, COUNT(*) as NumberOfDocs
 /*d.Id As EDIDocId, d.CreatedTimestamp, d.ChangedTimestamp, t.Created, edi.SavedDocument_id As RealDocId*/
 From [Medius.Core.Entities.Integration].EDIDocumentImport edi Inner Join [Medius.Data].Document d On(d.Id = edi.Document_id) 
  Inner Join [Medius.Core.Entities].DocumentTask dt On(dt.Document_id = d.Id) Inner Join [Medius.Data].Task task On(task.Id = dt.Task_id)
@@ -12,7 +12,7 @@ Group By LEFT(CONVERT(varchar, d.CreatedTimestamp,112),6)
 
 --Expense invoice import speed
 --Assumption that it is tracked to documentIsRegistered
-Select 'Expense docs: ', LEFT(CONVERT(varchar, d.CreatedTimestamp,112),6) As YearMonth, AVG(DATEDIFF(MILLISECOND, d.CreatedTimestamp, task.Created)) as DiffMs, COUNT(*) as NumberOfDocs
+Select 'Expense docs: ', LEFT(CONVERT(varchar, d.CreatedTimestamp,112),6) As YearMonth, AVG(DATEDIFF(MILLISECOND, d.CreatedTimestamp, task.Created)) as ImportTimeMs, COUNT(*) as NumberOfDocs
 /*d.Id as DocumentId, d.CreatedTimestamp, d.ChangedTimestamp, task.Created, */
 from [Medius.Data].Document d Inner Join [Medius.PurchaseToPay.Entities].SupplierInvoice si On(si.Document_id = d.Id)
 Inner Join [Medius.ExpenseInvoice.Entities].ExpenseInvoice e On(e.SupplierInvoice_id = d.Id)
@@ -23,7 +23,7 @@ Group By LEFT(CONVERT(varchar, d.CreatedTimestamp,112),6)
 
 --Match invoice import speed
 --Assumption that it is tracked to document is registered
-Select 'Match docs: ', LEFT(CONVERT(varchar, d.CreatedTimestamp,112),6) As YearMonth, AVG(DATEDIFF(MILLISECOND, d.CreatedTimestamp, task.Created)) as DiffMs, COUNT(*) as NumberOfDocs
+Select 'Match docs: ', LEFT(CONVERT(varchar, d.CreatedTimestamp,112),6) As YearMonth, AVG(DATEDIFF(MILLISECOND, d.CreatedTimestamp, task.Created)) as ImportTimeMs, COUNT(*) as NumberOfDocs
 /*d.Id as DocumentId, d.CreatedTimestamp, d.ChangedTimestamp, task.Created, */
 from [Medius.Data].Document d Inner Join [Medius.PurchaseToPay.Entities].SupplierInvoice si On(si.Document_id = d.Id)
 Inner Join [Medius.OrderbasedInvoice.Entities].OrderbasedInvoice o On(o.SupplierInvoice_id = d.Id)
