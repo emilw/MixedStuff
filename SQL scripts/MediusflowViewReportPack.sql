4bfa650a0014587432b7455cf9bb8ba6d40869a9
emilw <emil@fornby.com>
2016-06-22 09:14:47 +0200

    Updated views with CreatedStamp and person who closed workflow step

--- SQL scripts/MediusflowViewReportPack.sql
+++ SQL scripts/MediusflowViewReportPack.sql
@@ -18,8 +18,9 @@ Go
 Create View [Medius.Reporting].SupplierInvoice as
 Select si.Document_id As DocumentId, si.InvoiceNumber as InvoiceNumber, si.InvoiceDate as InvoiceDate, c.Name as CompanyName,
 c.CompanyId As ERPCompanyId, s.Name As SupplierName, s.SupplierId As ERPSupplierId, si.Gross_Number_Value, curr.Code as CurrencyCode,
-p.Name as PaymentTerm
+p.Name as PaymentTerm, d.CreatedTimestamp As CreatedTimestamp
 From [Medius.PurchaseToPay.Entities].SupplierInvoice si
+	Inner Join [Medius.Data].Document d On(d.Id = si.Document_id)
 	Inner Join [Medius.Core.Entities].Company c On(c.Id = si.Company_id)
 	Inner Join [Medius.Enterprise.Entities].Supplier s On(s.Id = si.Supplier_id)
 	Inner Join [Medius.Core.Entities].Currency curr On(curr.Id = si.Gross_Currency_id)
@@ -37,8 +38,11 @@ Go
 
 Create View [Medius.Reporting].WorkflowStep as
 Select dt.Document_id as DocumentId, t.State as State, t.CreatedTimestamp as TaskCreated, t.ChangedTimestamp as TaskClosed,
-te.Value as WorkflowStateName, lang.Language as Languagae, DATEDIFF(MIN
+te.Value as WorkflowStateName, lang.Language as Language, u.FirstName As ClosedByUserFirstName, u.LastName As ClosedByUserLastName, u.UserName As ClosedByUserUserName
 From [Medius.Core.Entities].[DocumentTask] dt
 Inner Join [Medius.Data].Task t On(t.Id = dt.Task_id)
 Inner Join [Medius.Data].TranslationEntry te On(te.TranslationKey = t.Description)
 Inner Join [Medius.Data].Translation lang On(lang.Language = te.Language)
+Left Outer Join [Medius.Core.Entities.Workflow].[UserTask] ut On(ut.DocumentTask_id = dt.Task_id)
+Left Outer Join [Medius.Core.Entities].[User] u On(u.Id = ut.ClosedBy_id)
+Where lang.Language = 'En-us'
